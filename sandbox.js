e1ab417d0104e9f42966ce396d404faadbdb6cf7
Pyae Sone Phyo <95358418+im-PyaeSonePhyo@users.noreply.github.com>
2026-08-20 04:23:02 +0000

    lesson-5: Dynamic (any) Types

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,18 @@
 "use strict";
-let character = 'mario';
-let age;
-let isLoggedIn;
-// age = 'luigi';
-age = 30;
-// isLoggedIn = 25;
-isLoggedIn = true;
-// arrays
-let ninjas = [];
-ninjas.push('ryu');
-ninjas.push('chun-li');
-console.log(ninjas);
-// union types
+let age = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'luigi' };
+console.log(age);
 let mixed = [];
-mixed.push('hello');
+mixed.push(5);
+mixed.push('mario');
 mixed.push(false);
-mixed.push(20);
 console.log(mixed);
-let uid;
-// objects
-let ninjaOne;
-ninjaOne = { name: 'yoshi', age: 30 };
-let ninjaTwo;
-ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
+let ninja;
+ninja = { name: 'yoshi', age: 25 };
+console.log(ninja);
+ninja = { name: 25, age: 'yoshi' };
+console.log(ninja);
