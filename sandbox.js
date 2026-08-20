22a8b1e1a5d60d026365ff8482fe15124900a7c9
Pyae Sone Phyo <95358418+im-PyaeSonePhyo@users.noreply.github.com>
2026-08-20 04:15:31 +0000

    lesson-4: Explicit Types

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,25 @@
 "use strict";
+let character = 'mario';
+let age;
+let isLoggedIn;
+// age = 'luigi';
+age = 30;
+// isLoggedIn = 25;
+isLoggedIn = true;
 // arrays
-let names = ['luigi', 'mario', 'yoshi'];
-names.push('toad');
-// names.push(3);
-// names[1] = 3;
-let numbers = [10, 20, 12, 15];
-numbers.push(25);
-// numbers.push('shaun');
-// numbers[0] = 'shaun';
-let mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+let ninjas = [];
+ninjas.push('ryu');
+ninjas.push('chun-li');
+console.log(ninjas);
+// union types
+let mixed = [];
+mixed.push('hello');
+mixed.push(false);
+mixed.push(20);
+console.log(mixed);
+let uid;
 // objects
-let ninja = {
-    name: 'mario',
-    belt: 'black',
-    age: 30
-};
-ninja.age = 40;
-ninja.name = 'ryu';
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
-    age: 40,
-    // skills: ['running'],
-};
+let ninjaOne;
+ninjaOne = { name: 'yoshi', age: 30 };
+let ninjaTwo;
+ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
