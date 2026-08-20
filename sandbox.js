3b98dbaac3aef6160f1686fd91a7bd35b4dd810f
Pyae Sone Phyo <95358418+im-PyaeSonePhyo@users.noreply.github.com>
2026-08-20 03:55:03 +0000

    lesson-2: Type Basics

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,15 @@
 "use strict";
-const character = 'mario';
-console.log(character);
-const inputs = document.querySelectorAll('input');
-inputs.forEach(input => {
-    console.log(input);
-});
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+// character = 20;
+character = 'luigi';
+// age = 'yoshi';
+age = 40;
+// isBlackBelt = 'yes';
+isBlackBelt = true;
+const circ = (diameter) => {
+    return diameter * Math.PI;
+};
+// console.log(area('hello'));
+console.log(circ(7.5));
