05a988e9893bf5f430d30eb0556ad1c9a8f4dc14
Pyae Sone Phyo <95358418+im-PyaeSonePhyo@users.noreply.github.com>
2026-08-21 04:25:18 +0000

    lesson-7: Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log('testing');
+// let greet: Function = () => {
+//   console.log('hello, world');
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+// greet = 'hello';
+// greet = () => {
+//   console.log('hello, again');
+// }
+const add = (a, b, c /*?*/ = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 'ninja');
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+console.log(result);
+//# sourceMappingURL=sandbox.js.map
